dca87ec5057ad904e1b6789c1c5a4f32f3d060c4
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-07-02 23:17:22 +1200

    feat: upstream merge (#1530)

--- lua/options.lua
+++ lua/options.lua
@@ -2,17 +2,13 @@
 --
 -- NOTE: You can change these options as you wish!
 -- For more options, you can see `:help option-list`
-
-local opt = vim.opt
-
--- General settings
-opt.encoding = 'utf-8'                -- Set character set to UTF-8
-opt.fileformat = 'unix'               -- Set end of line character to LF
-opt.shiftwidth = 2                     -- Set the number of spaces per indentation level
-opt.tabstop = 2                        -- Set the number of spaces a tab counts for
-opt.expandtab = true                   -- Use spaces instead of tabs
-opt.autoindent = true                  -- Enable automatic indentation
-opt.smartindent = true                 -- Enable smart indentation
+vim.o.encoding = 'utf-8'                -- Set character set to UTF-8
+vim.opt.fileformat = 'unix'               -- Set end of line character to LF
+vim.o.shiftwidth = 2                     -- Set the number of spaces per indentation level
+vim.o.tabstop = 2                        -- Set the number of spaces a tab counts for
+vim.o.expandtab = true                   -- Use spaces instead of tabs
+vim.o.autoindent = true                  -- Enable automatic indentation
+vim.o.smartindent = true                 -- Enable smart indentation
 -- opt.insert_final_newline = true      -- Remove or comment out this line
 
 -- Trim trailing whitGespace on save for all file types
@@ -26,67 +22,73 @@ vim.cmd [[
 ]]
 
 -- Enable line wrapping for LSP diagnostics
-vim.opt.wrap = true
-vim.opt.linebreak = true
+vim.o.wrap = true
+vim.o.linebreak = true
 
 -- Make line numbers default
-opt.number = true
+vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 -- Experiment for yourself to see if you like it!
-opt.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-opt.mouse = 'a'
+vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-opt.showmode = false
+vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 -- Schedule the setting after `UiEnter` because it can increase startup-time.
 -- Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
 vim.schedule(function()
-  opt.clipboard = 'unnamedplus'
+  vim.o.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
-opt.breakindent = true
+vim.o.breakindent = true
 
 -- Save undo history
-opt.undofile = true
+vim.o.undofile = true
 
 -- Enable spell checking
-opt.spell = true
-opt.spelllang = { 'en_us' }
+vim.o.spell = true
+vim.opt.spelllang = { 'en_us' }
 -- TODO: Figure out why the below line is not working
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-opt.ignorecase = true
-opt.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-opt.signcolumn = 'yes'
+vim.o.signcolumn = 'yes'
 
 -- Decrease update time
-opt.updatetime = 250
+vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
-opt.timeoutlen = 300
+vim.o.timeoutlen = 300
 
 -- Configure how new splits should be opened
-opt.splitright = true
-opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 -- See `:help 'list'` and `:help 'listchars'`
-opt.list = true
-opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-opt.inccommand = 'split'
+vim.o.inccommand = 'split'
 
 -- Show which line your cursor
+vim.o.cursorline = false
 
 -- Set which line starts page scrolling
-opt.scrolloff = 8
+vim.o.scrolloff = 8
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
