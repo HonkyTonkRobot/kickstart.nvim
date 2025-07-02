b5fe03c26d2a118310b4a73443218d5ce98f20a5
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-07-02 23:26:23 +1200

    style: clean up formatting

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -322,14 +322,14 @@ end
 --    See `:help CursorHold` for information about when this is executed
 --
 -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
+local client = vim.lsp.get_client_by_id(event.data.client_id)
+if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+buffer = event.buf,
+group = highlight_augroup,
+callback = vim.lsp.buf.document_highlight,
+})
 
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 buffer = event.buf,
@@ -350,48 +350,48 @@ end
 -- code, if the language server you are using supports them
 --
 -- This may be unwanted, since they displace some of your code
-          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
+if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+map('<leader>th', function()
+vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+end, '[T]oggle Inlay [H]ints')
+end
+end,
+})
 
-      -- Diagnostic Config
-      -- See :help vim.diagnostic.Opts
-      vim.diagnostic.config {
-        severity_sort = true,
-        float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
-          text = {
-            [vim.diagnostic.severity.ERROR] = '󰅚 ',
-            [vim.diagnostic.severity.WARN] = '󰀪 ',
-            [vim.diagnostic.severity.INFO] = '󰋽 ',
-            [vim.diagnostic.severity.HINT] = '󰌶 ',
-          },
-        } or {},
-        virtual_text = {
-          source = 'if_many',
-          spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
-        },
-      }
+-- Diagnostic Config
+-- See :help vim.diagnostic.Opts
+vim.diagnostic.config {
+severity_sort = true,
+float = { border = 'rounded', source = 'if_many' },
+underline = { severity = vim.diagnostic.severity.ERROR },
+signs = vim.g.have_nerd_font and {
+text = {
+[vim.diagnostic.severity.ERROR] = '󰅚 ',
+[vim.diagnostic.severity.WARN] = '󰀪 ',
+[vim.diagnostic.severity.INFO] = '󰋽 ',
+[vim.diagnostic.severity.HINT] = '󰌶 ',
+},
+} or {},
+virtual_text = {
+source = 'if_many',
+spacing = 2,
+format = function(diagnostic)
+local diagnostic_message = {
+[vim.diagnostic.severity.ERROR] = diagnostic.message,
+[vim.diagnostic.severity.WARN] = diagnostic.message,
+[vim.diagnostic.severity.INFO] = diagnostic.message,
+[vim.diagnostic.severity.HINT] = diagnostic.message,
+}
+return diagnostic_message[diagnostic.severity]
+end,
+},
+}
 
 -- LSP servers and clients are able to communicate to each other what features they support.
 --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -518,73 +518,73 @@ dependencies = {
 },
 opts = {}
 },
-      'folke/lazydev.nvim',
-    },
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
-    opts = {
-      keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+'folke/lazydev.nvim',
+},
+--- @module 'blink.cmp'
+--- @type blink.cmp.Config
+opts = {
+keymap = {
+-- 'default' (recommended) for mappings similar to built-in completions
+--   <c-y> to accept ([y]es) the completion.
+--    This will auto-import if your LSP supports it.
+--    This will expand snippets if the LSP sent a snippet.
+-- 'super-tab' for tab to accept
+-- 'enter' for enter to accept
+-- 'none' for no mapping
+--
+-- For an understanding of why the 'default' preset is recommended,
+-- you will need to read `:help ins-completion`
+--
+-- No, but seriously. Please read `:help ins-completion`, it is really good!
+--
+-- All presets have the following mappings:
+-- <tab>/<s-tab>: move to right/left of your snippet expansion
+-- <c-space>: Open menu or open docs if already open
+-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+-- <c-e>: Hide menu
+-- <c-k>: Toggle signature help
+--
+-- See :h blink-cmp-config-keymap for defining your own keymap
+preset = 'default',
 
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      },
+-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+},
 
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
+appearance = {
+-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+-- Adjusts spacing to ensure icons are aligned
+nerd_font_variant = 'mono',
+},
 
-      completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      },
+completion = {
+-- By default, you may press `<c-space>` to show the documentation.
+-- Optionally, set `auto_show = true` to show the documentation after a delay.
+documentation = { auto_show = false, auto_show_delay_ms = 500 },
+},
 
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
-        providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-      },
+sources = {
+default = { 'lsp', 'path', 'snippets', 'lazydev' },
+providers = {
+lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+},
+},
 
-      snippets = { preset = 'luasnip' },
+snippets = { preset = 'luasnip' },
 
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+-- which automatically downloads a prebuilt binary when enabled.
+--
+-- By default, we use the Lua implementation instead, but you may enable
+-- the rust implementation via `'prefer_rust_with_warning'`
+--
+-- See :h blink-cmp-config-fuzzy for more information
+fuzzy = { implementation = 'lua' },
 
-      -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
-    },
-  },
+-- Shows a signature help window while you type arguments for a function
+signature = { enabled = true },
+},
+},
 
 {
 -- Change the name of the colorscheme plugin below, and then
@@ -689,7 +689,8 @@ indent = { enable = true, disable = { "ruby" } },
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
-},{
+},
+{
   "joshuavial/aider.nvim",
   opts = {
     -- your configuration comes here
