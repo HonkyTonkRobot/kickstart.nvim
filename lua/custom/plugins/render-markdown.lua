f6bb6de299d41632e92007de7ea9b31f70f65e85
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-08-14 23:24:32 +1200

    fix: render-markdown treesitter parsing conflict

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,12 +1,26 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  ft = { 'markdown' }, -- Only load for markdown files
-  opts = {
-    completions = { lsp = { enabled = true } },
-  },
+  ft = { 'markdown' },
+  config = function()
+    -- Disable initially to avoid C-call boundary error
+    require('render-markdown').setup({
+      enabled = false,  -- Start disabled
+      completions = { lsp = { enabled = true } },
+    })
+    
+    -- Enable after treesitter is fully ready
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.schedule(function()
+          require('render-markdown').enable()
+        end)
+      end,
+    })
+  end,
   keys = {
     {
       '<leader>tm',
@@ -18,4 +32,3 @@ return {
     },
   },
 }
-
