9c8df5de9a8fb1e35019fe4983a0b02b7db921c7
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-08-14 22:40:44 +1200

    refactor: clean up unused code, unneeded files

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -3,5 +3,19 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  ft = { 'markdown' }, -- Only load for markdown files
+  opts = {
+    completions = { lsp = { enabled = true } },
+  },
+  keys = {
+    {
+      '<leader>tm',
+      function()
+        require('render-markdown').toggle()
+      end,
+      desc = '[T]oggle [M]arkdown rendering',
+      ft = 'markdown',
+    },
+  },
 }
+
