04f2bc4cb1b5b268f729f262cd6f4331a273b4be
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-08-14 20:57:13 +1200

    feat: extract tokyonight colorscheme to seperate plugin file

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -703,22 +703,6 @@ require("lazy").setup({
     },
   },
 
-  {
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    -- colorschemes already installed, use `:Telescope colorscheme`.
-    "folke/tokyonight.nvim",
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme("tokyonight-night")
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi("Comment gui=none")
-    end,
-  },
 
   -- Highlight todo, notes, etc in comments
   {
@@ -808,7 +792,7 @@ require("lazy").setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
