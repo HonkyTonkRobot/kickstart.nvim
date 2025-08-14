636423407a67c4e33de037a75e056dd60e879639
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-08-14 22:19:21 +1200

    feat: move blink cmp and luasnip to their own files and add friendly snippet library to luasnip

---
+++ lua/custom/plugins/blink-cmp.lua
@@ -0,0 +1,44 @@
+return { -- Autocompletion
+  'saghen/blink.cmp',
+  event = 'VimEnter',
+  version = '1.*',
+  dependencies = {
+    -- Snippet Engine
+    "L3MON4D3/LuaSnip",
+    'folke/lazydev.nvim',
+  },
+  --- @module 'blink.cmp'
+  --- @type blink.cmp.Config
+  opts = {
+    keymap = {
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      preset = 'default',
+
+      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+      --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+    },
+
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+
+    completion = {
+      -- By default, you may press `<c-space>` to show the documentation.
+      -- Optionally, set `auto_show = true` to show the documentation after a delay.
+      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+    },
+
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      providers = {
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+      },
+    },
+
+    snippets = { preset = 'luasnip' },
+    -- TODO: [fast] rust implementation
+    fuzzy = { implementation = 'lua' },
+    signature = { enabled = true },
+  },
+}
+
